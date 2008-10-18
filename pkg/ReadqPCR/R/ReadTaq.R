12e2939ef122857bbd1010e9d75fcd0e9147b847
stamats (Prof. Dr. ) <Matthias.Kohl@stamats.de>
2008-10-18 08:02:38 +0000

    package ReadqPCR checks and installs without any errors or warnings using R version 2.8.0 RC (2008-10-14 r46714) and R version 2.9.0 Under development (unstable) (2008-10-14 r46714).
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/qpcr@9 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/ReadqPCR/R/ReadTaq.R
+++ pkg/ReadqPCR/R/ReadTaq.R
@@ -1,20 +1,18 @@
-`ReadTaq` <-
-function(filename = character()) {
-
+ReadTaq <- function(filename = character()) {
 # If no filename given look for .taq files in current directory
 if (length(filename) == 0) {
-	taqfile.path <- getwd()
-	files = list.files(taqfile.path)
-	filename = files[grep("\\.[tT][aA][qQ]$",files)]
-	if (length(filename)>1) {
-		stop("Too many Taq files in current directory","\n")
-	}
+    taqfile.path <- getwd()
+    files = list.files(taqfile.path)
+    filename = files[grep("\\.[tT][aA][qQ]$",files)]
+    if (length(filename)>1) {
+      stop("Too many Taq files in current directory","\n")
+    }
 }
 if (length(filename) == 0) {
-	stop("No Taqman filenname specified and no Taqman files in current directory:","\n")
+    stop("No Taqman filenname specified and no Taqman files in current directory:","\n")
 }
 if (length(filename)>1) {
-	stop ("Too may Taqman files specified")
+    stop ("Too may Taqman files specified")
 }
 taq2eset(filename)
 }
--- pkg/ReadqPCR/R/taq2eset.R
+++ pkg/ReadqPCR/R/taq2eset.R
@@ -1,18 +1,18 @@
-`taq2eset` <-
-function(filename) {
-raw = read.delim(filename)
-levels(raw$Sample) = gsub(" ","_",levels(raw$Sample))
-samples = levels(raw$Sample)
-detectors = levels(raw$Detector)
+taq2eset <- function(filename){
+    raw <- read.delim(filename)
+    levels(raw$Sample) <- gsub(" ","_",levels(raw$Sample))
+    samples <- levels(raw$Sample)
+    detectors <- levels(raw$Detector)
 
-eset = data.frame(detectors,row.names=1)
-for (sample in samples) {
-	Cts = data.frame(raw$Detector[raw$Sample == sample],as.numeric(as.character(raw$Ct[raw$Sample == sample])),row.names=1)
-	eset = data.frame(merge(eset,Cts,by="row.names"),row.names=1)
+    eset <- data.frame(detectors,row.names=1)
+    for (sample in samples) {
+       Cts <- data.frame(raw$Detector[raw$Sample == sample], 
+                         as.numeric(as.character(raw$Ct[raw$Sample == sample])), 
+                         row.names=1)
+       eset <- data.frame(merge(eset,Cts, by="row.names"), row.names=1)
+    }
+    names(eset) <- samples
+    eset <- new("ExpressionSet", exprs=as.matrix(eset)) # make it an expression set
+
+    return(eset)
 }
-names(eset) = samples
-eset = new("ExpressionSet",exprs=as.matrix(eset)) # make it an expression set
-
-return(eset)
-}
-
