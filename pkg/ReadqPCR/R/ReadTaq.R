b6944cf42169010bb31be859faa5c0123e647edf
jperkins (Mr ) <jperkins@biochem.ucl.ac.uk>
2009-01-28 17:58:23 +0000

    prevent bugs when multiple replicates encountered (although still can't handle them
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/qpcr@19 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/ReadqPCR/R/ReadTaq.R
+++ pkg/ReadqPCR/R/ReadTaq.R
@@ -4,16 +4,20 @@ if (length(filename) == 0) {
     taqfile.path <- getwd()
     files = list.files(taqfile.path)
     filename = files[grep("\\.[tT][aA][qQ]$",files)]
-    if (length(filename)>1) {
+    if (length(filename) > 1) {
       stop("Too many Taq files in current directory","\n")
     }
 }
 if (length(filename) == 0) {
     stop("No Taqman filenname specified and no Taqman files in current directory:","\n")
 }
-if (length(filename)>1) {
-    stop ("Too may Taqman files specified")
+if (length(filename) > 1) {
+    stop("Too may Taqman files specified")
 }
-taq2eset(filename)
+	tSet = taq2eset(filename)
+	if (class(tSet) != "ExpressionSet") {
+		stop("Error converting data into Expression Set")
+	}
+	return(tSet)
 }
 
--- pkg/ReadqPCR/R/taq2eset.R
+++ pkg/ReadqPCR/R/taq2eset.R
@@ -1,13 +1,32 @@
 taq2eset <- function(filename){
     raw <- read.delim(filename)
-    levels(raw$Sample) <- gsub(" ","_",levels(raw$Sample))
+    levels(raw$Sample) <- gsub(" ","_",levels(raw$Sample)) # replace spaces with _ for sample names
+    levels(raw$Detector) <- gsub(" ","_",levels(raw$Detector)) # replace spaces with _ for detectors
+
     samples <- levels(raw$Sample)
     detectors <- levels(raw$Detector)
 
     eset <- data.frame(detectors,row.names=1)
     for (sample in samples) {
-       Cts <- data.frame(raw$Detector[raw$Sample == sample], 
-                         as.numeric(as.character(raw$Ct[raw$Sample == sample])), 
+       # work out if there are technical replicates
+       total_detectors = length(raw$Detector[raw$Sample == sample])
+       individual_detectors = length(levels(raw$Detector[raw$Sample == sample]))
+
+       tech_reps = total_detectors/individual_detectors
+
+       if ((tech_reps %% 1) != 0) { # if total number of replicates not a multiple of number of individual detectors
+          warning_text = paste("Corrupt taqman file: total number of readings for sample ",sample," not a multiple of number of individual detectors")
+          warning(warning_text)
+          return(0)
+
+       }
+       if (tech_reps > 1) { # Currently can't cope with technical replicates
+          warning_text = "More than 1 technical replicate detected"
+          warning(warning_text)
+          return(0)
+       }
+       Cts <- data.frame(raw$Detector[raw$Sample == sample],
+                         as.numeric(as.character(raw$Ct[raw$Sample == sample])),
                          row.names=1)
        eset <- data.frame(merge(eset,Cts, by="row.names"), row.names=1)
     }
