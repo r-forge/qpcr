f4bea42ac14d8c1a4faa967273f4b0d78a01f875
jperkins (Mr ) <jperkins@biochem.ucl.ac.uk>
2009-02-04 18:59:06 +0000

    slight changes
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/qpcr@26 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/ReadqPCR/R/affybatch.R
+++ pkg/ReadqPCR/R/affybatch.R
@@ -3,9 +3,12 @@ read.taqmanbatch <- function(..., filenames = character(0), phenoData = new("Ann
     auxnames <- unlist(list(...))
     filenames <- c(filenames, auxnames)
     checkValidTaqmanFilenames(filenames)
-    pdata <- pData(phenoData) # number of files
-    exprs <- read_TaqBatch(filenames, verbose)
-    if (dim(pdata)[1] != n) {
+    pdata <- pData(phenoData) # number of filesi
+    exprs <- read_TaqBatch(filenames, verbose) # need to make this work for tech reps and multiple files
+	n = 1
+    n <- length(colnames(exprs))
+	cat("ENNNNNNNNNNNNNNNNNNNYA")
+    if (dim(pdata)[1] != n) { # so if we don't have a row for each sample in the pData matrix
         warning("Incompatible phenoData object. Created a new one using sample name data derived from raw data.\n")
         samplenames <- sub("^/?([^/]*/)*", "", colnames(exprs), extended = TRUE)
         pdata <- data.frame(sample = 1:length(samplenames), row.names = samplenames)
@@ -15,6 +18,7 @@ read.taqmanbatch <- function(..., filenames = character(0), phenoData = new("Ann
     }
     return(new("ExpressionSet", exprs = exprs, phenoData = phenoData))
 }
+
 read_TaqBatch <- function(filenames, verbose)
 {
     for (filename in filenames) {
@@ -45,13 +49,13 @@ read_TaqBatch <- function(filenames, verbose)
                          as.numeric(as.character(raw$Ct[raw$Sample == sample])),
                          row.names=1)
             exprs <- data.frame(merge(exprs, Cts, by="row.names"), row.names=1)
+            if (verbose) cat("sample ", sample, "read\n")
         }
         names(exprs) <- samples
         exprs <- as.matrix(exprs)
     }
     return(exprs)
 }
-
 checkValidTaqmanFilenames <- function (filenames)
 {
     if (!is.character(filenames))
