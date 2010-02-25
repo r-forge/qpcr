757c13cf9a2424aa390ebb417c009a2a21f0fc0a
jperkins (Mr ) <jperkins@biochem.ucl.ac.uk>
2010-02-25 18:20:36 +0000

    Can normalise without comparing - could be useful for pseudocard perhaps
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/qpcr@95 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/NormqPCR/R/deltaCt.R
@@ -0,0 +1,36 @@
+setGeneric("deltaCt",
+  function(qPCRBatch, hkgs)
+  standardGeneric("deltaCt")
+)
+setMethod("deltaCt", signature = "qPCRBatch", definition =
+  function(qPCRBatch, hkgs) {
+    hkgs <- make.names(hkgs)
+    if(FALSE %in% (hkgs %in% featureNames(qPCRBatch))) stop ("given housekeeping gene, ", hkgs," not found in file. Ensure entered housekeeping genes appear in the file")
+    expM <- exprs(qPCRBatch)
+    hkgM <- expM[hkgs, ]
+    print(hkgM)
+
+    if(length(hkgs) > 1) {
+      if (TRUE %in% apply(hkgM, 1, is.na))  {
+          warning("NAs present in housekeeping genes readings")
+          if (0 %in% apply(! apply(hkgM, 1, is.na),2,sum)) stop("Need at least 1 non NA for each housekeeper")
+      }
+#     hkgV <- apply(hkgM,2,geomMean,na.rm=TRUE) CANT CURRENTLY DO THIS BECAUS EOF PROBLEMS WITH DOUBLE NAS
+      hkgV <- vector(length = dim(hkgM)[2])
+      for(i in 1:dim(hkgM)[2]) {
+          if(! FALSE %in% is.na(hkgM[,i])) next # go to next sequence if we have only NAs
+          hkgV[i] <- geomMean(hkgM[,i], na.rm=TRUE)
+      }
+    }
+    else {
+      if(TRUE %in% is.na(hkgM)) {
+          warning("NAs present in housekeeping gene readings")
+          if(! FALSE %in% is.na(hkgM)) stop("Need at least 1 non NA for the housekeeper")
+      }
+      hkgV <- hkgM # Because it's a vector really anyway
+      cat(hkgV)
+    }
+    exprs(qPCRBatch) <- 2^-(t(t(exprs(qPCRBatch)) - hkgV))
+    return(qPCRBatch)
+  }
+)
