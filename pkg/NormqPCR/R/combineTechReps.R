21652af85f5beb9f46039373bffaf76a6f934205
jperkins (Mr ) <jperkins@biochem.ucl.ac.uk>
2010-02-25 17:50:24 +0000

    now can combine technical replicates using mean
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/qpcr@93 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/NormqPCR/R/combineTechReps.R
@@ -0,0 +1,20 @@
+setGeneric("combineTechReps",
+  function(qPCRBatch)
+  standardGeneric("combineTechReps")
+)
+setMethod("combineTechReps", signature = "qPCRBatch", definition =
+  function(qPCRBatch) {
+    expM <- exprs(qPCRBatch)
+    origDetectors <- row.names(expM)
+    if (FALSE %in% grepl("_TechReps", origDetectors)) stop("These are not tech reps")
+    newDetectors <- unique(gsub("_TechReps.\\d","", origDetectors))
+
+    NewExpM <- matrix(nrow = length(newDetectors), ncol = dim(expM)[2], dimnames = list(newDetectors,colnames(expM)))
+
+    for(detector in newDetectors){
+      dValues <- as.numeric(apply(expM[grepl(detector, origDetectors),],2,mean,na.rm=TRUE))
+      NewExpM[detector,] <- dValues
+    }
+    qPCRBatch <- new("qPCRBatch", exprs = NewExpM, phenoData = phenoData(qPCRBatch))
+  }
+)
