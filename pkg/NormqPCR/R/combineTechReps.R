0a2a5670a2e540af274939ce8ca531c04c96c082
jperkins (Mr ) <jperkins@biochem.ucl.ac.uk>
2010-09-09 17:35:35 +0000

    Ridiculously difficult to find bug in combineTechReps, was grepping very lazily previously
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/qpcr@123 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/NormqPCR/R/combineTechReps.R
+++ pkg/NormqPCR/R/combineTechReps.R
@@ -10,7 +10,10 @@ setMethod("combineTechReps", signature = "qPCRBatch", definition =
     newDetectors <- unique(gsub("_TechReps.\\d","", origDetectors))
     NewExpM <- matrix(nrow = length(newDetectors), ncol = dim(expM)[2], dimnames = list(newDetectors,colnames(expM)))
     for(detector in newDetectors){
+	cat(detector,"\t")
+        cat(expM[gsub("_TechReps.\\d","", origDetectors) %in% origDetectors],"\t")
       dValues <- colMeans(expM[grepl(detector, origDetectors),],na.rm=TRUE)
+	cat(dValues,"\n")
       NewExpM[detector,] <- dValues
     }
     NewExpM[is.na(NewExpM)] <- NA
