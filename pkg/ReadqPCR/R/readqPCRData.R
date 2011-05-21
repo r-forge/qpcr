e9e43b31fba59318b371b526b2cf204700b7645d
jperkins (Mr ) <jperkins@biochem.ucl.ac.uk>
2011-05-21 17:03:22 +0000

    Now meets expectations on bioconductor list. Next thing to do: check man pages. R CMD check Currently throwing an error due to lack of pdf file in the <pkg>inst/doc directory I think this is ok (mega thread on this in R-devel)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/qpcr@135 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/ReadqPCR/R/readqPCRData.R
+++ pkg/ReadqPCR/R/readqPCRData.R
@@ -1,32 +1,3 @@
-setClass("qPCRBatch", contains = "eSet")
-
-## accessor methods
-setMethod("exprs", signature = "qPCRBatch", definition = 
-    function (object) assayDataElement(object, "exprs")
-)
-
-setReplaceMethod("exprs", signature = "qPCRBatch", definition = 
-    function (object, value) assayDataElementReplace(object, "exprs", value)
-)
-
-setGeneric("exprs.well.order",
-    function(object)
-    standardGeneric("exprs.well.order")
-)
-
-setGeneric("exprs.well.order<-",
-    function(object, ..., value)
-    standardGeneric("exprs.well.order<-")
-)
-
-setMethod("exprs.well.order", signature = "qPCRBatch", definition = 
-    function (object) assayDataElement(object, "exprs.well.order")
-)
-
-setReplaceMethod("exprs.well.order", signature = "qPCRBatch", definition = 
-    function (object, value) assayDataElementReplace(object, "exprs.well.order", value)
-)
-
 read.taqman <- function(..., filenames = character(0), phenoData = new("AnnotatedDataFrame"), notes = "", verbose = FALSE)
 {
     auxnames <- unlist(list(...))
@@ -55,7 +26,7 @@ read.taqman <- function(..., filenames = character(0), phenoData = new("Annotate
     plate.offset <- 0 # this is used to name plates when combining several files 
     fileNameCount <- 1 # used to count number of files given
     for (filename in filenames) {
-        if (verbose) cat("Filename[ ",filename," ]")
+        if (verbose) message("Filename[ ",filename," ]")
         raw.data <- read.delim(filename, skip = 12) # read in file, ignoring the gubbins on the first 12 lines
         if (! 1 %in% regexpr("Summary", as.character(raw.data[,1]))) stop("Problems with Taqman file, Summary info not found") #
         EndOfData <- grep("Summary", raw.data[,1])
@@ -98,7 +69,7 @@ read.taqman <- function(..., filenames = character(0), phenoData = new("Annotate
         allDetectors <- raw.data$Detector # save these as a variable because raw.data$Detector will get changed
         firstTimeFlag <- TRUE
         for (sample in samples) { # for each sample
-            if (verbose) cat("Now reading for sample:", sample, "\n")
+            if (verbose) message("Now reading for sample:", sample, "\n")
             total.detectors <- length(allDetectors[raw.data$Sample == sample])
             individual.detectors <- length(unique(allDetectors[raw.data$Sample == sample]))
             tech.reps <- total.detectors/individual.detectors
@@ -108,7 +79,7 @@ read.taqman <- function(..., filenames = character(0), phenoData = new("Annotate
               stop(warning.text)
             }
             if (tech.reps > 1) { # Currently can't cope with technical replicates
-              if(verbose) cat ("More than 1 technical replicate detected\n")
+              if(verbose) message("More than 1 technical replicate detected\n")
               staticDetector <- raw.data$Detector[raw.data$Sample == sample] # need this since we are modifying raw.data$Detector
               for(techDetect in unique(raw.data$Detector[raw.data$Sample == sample]))  {
                 techDLength <- sum(staticDetector %in% techDetect)
@@ -118,7 +89,7 @@ read.taqman <- function(..., filenames = character(0), phenoData = new("Annotate
             }
             if(fileNameCount > 1) { # do some checking
               if(! FALSE %in% (sort(raw.data$Detector[raw.data$Sample == sample]) == sort(rownames(totalExprs)))) {
-                if(verbose == TRUE) cat("we are combining files with the same detector names\n")
+                if(verbose == TRUE) message("we are combining files with the same detector names\n")
               }
               else stop("Problem combining files on detector names. Make sure detector names match for all files\n")
               }#raw.data$Detector <- as.character(raw.data$Detector) # coerce to stop funny behaviour
@@ -140,7 +111,7 @@ read.taqman <- function(..., filenames = character(0), phenoData = new("Annotate
             if (! FALSE %in% (row.names(exprs) == row.names(Cts))) stop("BYE")
 
             well.order <- data.frame(merge(well.order, well.info, by="row.names"), row.names=1)
-            if (verbose) cat("sample ", sample, "read\n")
+            if (verbose) message("sample ", sample, "read\n")
         }
         names(well.order) <- samples
         names(exprs) <- samples
@@ -177,6 +148,120 @@ checkValidTaqmanFilenames <- function (filenames)
     if (any(whBad)) {
         msg <- paste("the following are not valid files:\n",
             paste("  ", filenames[whBad], collapse = "\n"))
+        stop(msg, call. = FALSE)
+    }
+    TRUE
+}
+
+read.qPCR <- function(filename = character(0), phenoData = new("AnnotatedDataFrame"), notes = "", verbose = FALSE)
+{
+    pdata <- pData(phenoData)
+    checkValidqPCRFilename(filename)
+    qPCRInfo <- .read.qPCR(filename, verbose) # need to make this work for tech reps and multiple files
+    exprs <- qPCRInfo$exprs
+    well.order <- qPCRInfo$well.order
+    exprs.well.order <- assayDataNew("environment", exprs.well.order = exprs)
+    n <- length(colnames(exprs))
+    if (dim(pdata)[1] != n) { # so if we don't have a row for each sample in the pData matrix
+        warning("Incompatible phenoData object. Created a new one using sample name data derived from raw data.\n")
+        samplenames <- sub("^/?([^/]*/)*", "", colnames(exprs))
+        pdata <- data.frame(sample = 1:length(samplenames), row.names = samplenames)
+        phenoData <- new("AnnotatedDataFrame", data = pdata,
+            varMetadata = data.frame(labelDescription = "arbitrary numbering",
+                row.names = "sample"))
+    }
+    if(! is.null(qPCRInfo$well.order)) {
+        return(new("qPCRBatch", exprs = exprs, phenoData = phenoData, exprs.well.order = well.order))
+    }
+    else {
+
+        return(new("qPCRBatch", exprs = exprs, phenoData = phenoData))
+    }
+}
+
+.read.qPCR <- function(filename, verbose)
+{
+    noWellData <- FALSE
+    raw.data <- read.table(filename, header=TRUE)
+    if(is.null(raw.data$Well) || is.null(raw.data$PlateID)) {
+         noWellData <- TRUE
+         if (verbose) message("No Well and/or Plate info found, skipping this part", "\n")
+    }
+    else {
+        raw.data$PlateID <- paste(raw.data$PlateID, as.character(raw.data$Well), sep= "-")
+    }
+    levels(raw.data$Sample) <- make.names(levels(raw.data$Sample))
+    levels(raw.data$Detector) <- make.names(levels(raw.data$Detector))
+    Ct <- as.character(raw.data$Ct)
+    samples <- levels(raw.data$Sample)
+    detectors <- levels(raw.data$Detector)
+    allDetectors <- raw.data$Detector
+    firstTimeFlag <- TRUE
+    for (sample in samples) { # for each sample
+        if (verbose) message("Now reading for sample:", sample, "\n")
+        total.detectors <- length(allDetectors[raw.data$Sample == sample])
+        individual.detectors <- length(levels(allDetectors[raw.data$Sample == sample]))
+        tech.reps <- total.detectors/individual.detectors
+        raw.data$Detector <- as.character(raw.data$Detector)
+          if ((tech.reps %% 1) != 0) { # if total number of replicates not a multiple of number of individual detectors
+            warning.text = paste("File incorrect, make sure that detectors are the same for all samples")
+            stop(warning.text)
+        }
+        if (tech.reps > 1) {
+            if(verbose) message("More than 1 technical replicate detected\n")
+              staticDetector <- raw.data$Detector[raw.data$Sample == sample]
+              for(techDetect in unique(raw.data$Detector[raw.data$Sample == sample]))  {
+                techDLength <- sum(staticDetector %in% techDetect)
+                suffixedNames <- paste(techDetect, 1:techDLength, sep="_TechReps.")
+                raw.data$Detector[raw.data$Sample == sample][raw.data$Detector[raw.data$Sample == sample] 
+                  %in% techDetect] <- suffixedNames
+            }
+       }
+       if(firstTimeFlag == TRUE) {
+           exprs <- data.frame(unique(raw.data$Detector), row.names=1) # start the exprs data frame
+           well.order <- data.frame(unique(raw.data$Detector), row.names=1)
+           firstTimeFlag <- FALSE
+       }
+       raw.data$Detector <- as.factor(raw.data$Detector)
+        if(noWellData == FALSE) {
+            well.info <- data.frame(raw.data$Detector[raw.data$Sample == sample], # put well info values in a matrix
+              raw.data$PlateID[raw.data$Sample == sample],
+                row.names=1)
+        }
+        Cts <- data.frame(raw.data$Detector[raw.data$Sample == sample],
+          as.numeric(as.character(raw.data$Ct[raw.data$Sample == sample])),
+            row.names=1)
+        exprs <- data.frame(merge(exprs, Cts, by="row.names"), row.names=1)
+        if(noWellData == FALSE) {
+            well.order <- data.frame(merge(well.order, well.info, by="row.names"), row.names=1)
+        }
+        if (verbose) message("sample ", sample, "read\n")
+    }
+    qPCRInfo <- list()
+    names(exprs) <- samples
+    qPCRInfo$exprs <- as.matrix(exprs)
+    if(noWellData == FALSE) {
+      colnames(well.order) <- names(exprs)
+      qPCRInfo$well.order <- well.order
+    }
+    return(qPCRInfo)
+}
+
+checkValidqPCRFilename <- function (filename)
+{
+    if (!is.character(filename))
+        stop(strwrap(paste("file name must be specified using a character",
+            "vector, not a", sQuote(typeof(filename)))), call. = FALSE)
+    if (length(filename) == 0)
+        stop("no file name provided")
+    if (any(sapply(filename, nchar) < 1))
+        stop("empty file name not allowed")
+    finfo <- file.info(filename)
+    whBad <- sapply(finfo[["isdir"]], function(x) !identical(FALSE,
+        x))
+    if (any(whBad)) {
+        msg <- paste("not valid file:\n",
+            paste("  ", filename[whBad], collapse = "\n"))
         stop(msg, call. = FALSE)
     }
     TRUE
